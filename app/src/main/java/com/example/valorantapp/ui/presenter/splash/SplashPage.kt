f1e9e6885a48b53dd7cf83e774b73a46d7bce212
Matheus Angelicio <matheus.angelicio00@gmail.com>
2024-05-22 01:28:32 -0300

    add lottie animation

--- app/src/main/java/com/example/valorantapp/ui/presenter/splash/SplashPage.kt
+++ app/src/main/java/com/example/valorantapp/ui/presenter/splash/SplashPage.kt
@@ -1,14 +1,46 @@
 package com.example.valorantapp.ui.presenter.splash
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.valorantapp.R
+
 
 @Composable
 fun SplashPage() {
 
-    Column {
-        Text(text = "Testando")
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primary
+    ){
+        SplashContent()
     }
 
+}
+
+@Composable
+fun SplashContent() {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(
+            resId = R.raw.splash_animation
+        )
+    )
+
+    LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SplashPagePreview() {
+    SplashPage()
 }
