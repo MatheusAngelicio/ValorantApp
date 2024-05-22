5bb63d51b77e854cd242007481f237bcfe4da65c
Matheus Angelicio <matheus.angelicio00@gmail.com>
2024-05-22 02:07:41 -0300

    add valorant image

--- app/src/main/java/com/example/valorantapp/ui/presenter/splash/SplashPage.kt
+++ app/src/main/java/com/example/valorantapp/ui/presenter/splash/SplashPage.kt
@@ -1,14 +1,23 @@
 package com.example.valorantapp.ui.presenter.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -19,15 +28,14 @@ import com.example.valorantapp.R
 @Composable
 fun SplashPage() {
 
-    Surface (
+    Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.primary
-    ){
+    ) {
         SplashContent()
     }
 
 }
-
 @Composable
 fun SplashContent() {
     val composition by rememberLottieComposition(
@@ -35,9 +43,21 @@ fun SplashContent() {
             resId = R.raw.splash_animation
         )
     )
+    Column{
+        Image(
+            painter = painterResource(id = R.drawable.valorant_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
 
-    LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever)
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
