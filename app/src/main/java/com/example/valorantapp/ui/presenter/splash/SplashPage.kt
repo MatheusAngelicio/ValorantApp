2cea00d896da00a1c832aa2b71738933f1961c41
Matheus Angelicio <matheus.angelicio00@gmail.com>
2024-05-22 00:07:22 -0300

    update theme and add initial splash page

---
+++ app/src/main/java/com/example/valorantapp/ui/presenter/splash/SplashPage.kt
@@ -0,0 +1,14 @@
+package com.example.valorantapp.ui.presenter.splash
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashPage() {
+
+    Column {
+        Text(text = "Testando")
+    }
+
+}
