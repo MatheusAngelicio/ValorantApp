b5e16f80868d35e57a270290e06e7305fff4bcdc
Matheus Angelicio <matheus.angelicio00@gmail.com>
2024-05-23 22:01:03 -0300

    add HomePage protorype

--- app/src/main/java/com/example/valorantapp/MainActivity.kt
+++ app/src/main/java/com/example/valorantapp/MainActivity.kt
@@ -3,14 +3,7 @@ package com.example.valorantapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.valorantapp.ui.presenter.splash.SplashPage
+import com.example.valorantapp.ui.presenter.home.HomePage
 import com.example.valorantapp.ui.theme.ValorantAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ValorantAppTheme {
-                SplashPage()
+                HomePage()
             }
         }
     }
---
+++ app/src/main/java/com/example/valorantapp/ui/presenter/home/HomePage.kt
@@ -0,0 +1,185 @@
+package com.example.valorantapp.ui.presenter.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.valorantapp.ui.theme.black
+import com.example.valorantapp.ui.theme.red
+import com.example.valorantapp.ui.theme.white
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage() {
+    data class NavigationItem(
+        val title: String,
+        val route: String,
+        val selectedIcon: ImageVector,
+        val unselectedIcon: ImageVector,
+        val badgeCount: Int? = null
+    )
+
+    val items = listOf(
+        NavigationItem(
+            title = "All",
+            route = "all",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        NavigationItem(
+            title = "Urgent",
+            route = "urgent",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Info,
+            badgeCount = 45
+        ),
+        NavigationItem(
+            title = "Settings",
+            route = "settings",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings
+        )
+    )
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = black
+    ) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+        val navController = rememberNavController()
+
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    items.forEachIndexed { index, item ->
+                        NavigationDrawerItem(
+                            label = { Text(text = item.title) },
+                            selected = index == selectedItemIndex,
+                            onClick = {
+                                selectedItemIndex = index
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                                navController.navigate(item.route)
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) {
+                                        item.selectedIcon
+                                    } else {
+                                        item.unselectedIcon
+                                    },
+                                    contentDescription = item.title
+                                )
+                            },
+                            badge = {
+                                item.badgeCount?.let {
+                                    Text(text = item.badgeCount.toString())
+                                }
+                            },
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )
+                    }
+                }
+            },
+            drawerState = drawerState
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text = "Valorant App") },
+                        navigationIcon = {
+                            IconButton(onClick = {
+                                scope.launch {
+                                    drawerState.open()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = "Menu",
+                                    tint = white
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = black,
+                            titleContentColor = white,
+                            actionIconContentColor = white
+                        )
+                    )
+                }
+            ) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = items[0].route
+                    ) {
+                        composable("all") { AllScreen() }
+                        composable("urgent") { UrgentScreen() }
+                        composable("settings") { SettingsScreen() }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AllScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(black))
+}
+
+@Composable
+fun UrgentScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(red))
+
+}
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "Settings Screen")
+}
